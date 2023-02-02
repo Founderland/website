ce3fa0d8234d6b7c7080dd3ab39df2a8b9b7e9dd
Victor Isidoro <31276085+vtr84@users.noreply.github.com>
2023-02-02 14:18:59 +0100

    color palette config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['*.{html,js}'],
   theme: {
     extend: {
       colors: {
         // brand
-        'fdl-blue': {
+        fdlblue: {
+          DEFAULT: '#0063e2',
           50: '#edf9ff',
           100: '#d6f0ff',
           200: '#b5e7ff',
@@ -17,30 +17,45 @@ module.exports = {
           800: '#0855c5',
           900: '#0d4b9b',
         },
-        red: { DEFAULT: '#F6331C' },
-        pink: { DEFAULT: '#EE93B5' },
-        lime: { DEFAULT: '#D7FB03' },
-        black: { DEFAULT: '#000' },
-        white: { DEFAULT: '#FFF' },
-
-        // R/GA COLORS DIFFER FROM FDL PLAYBOOK
-        // blue: {
-        //   DEFAULT: '#2565DA',
-        //   400: '#3885E8',
-        //   300: '#80B1F1',
-        //   200: '#B3D0F6',
-        //   100: '#D9E8FB',
-        // },
-        // red: { DEFAULT: '#E24630' },
-        // pink: { DEFAULT: '#E197B3' },
-        // lime: { DEFAULT: '#DFF851' },
-        // black: { DEFAULT: '#000' },
-        // white: { DEFAULT: '#FFF' },
-
-        // grayscale
-        'gray-dark': { DEFAULT: '#707070' },
-        gray: { DEFAULT: '#A4A4A4' },
-        'gray-light': { DEFAULT: '#F1F1F1' },
+        fdlred: {
+          DEFAULT: '#F6331C',
+          50: '#fff2f0',
+          100: '#ffe1de',
+          200: '#ffc9c3',
+          300: '#ffa499',
+          400: '#ff6f5e',
+          500: '#ff422c',
+          600: '#f6331c',
+          700: '#cf1b06',
+          800: '#ab1a09',
+          900: '#8d1c0f',
+        },
+        fdlpink: {
+          DEFAULT: '#EE93B5',
+          50: '#fcf3f7',
+          100: '#fae9f0',
+          200: '#f8d2e2',
+          300: '#f3aec9',
+          400: '#ee93b5',
+          500: '#e05484',
+          600: '#ce3461',
+          700: '#b2244a',
+          800: '#93213e',
+          900: '#7b2037',
+        },
+        fdllime: {
+          DEFAULT: '#D7FB03',
+          50: '#ffffe5',
+          100: '#fcffc6',
+          200: '#f7ff93',
+          300: '#edff55',
+          400: '#def922',
+          500: '#d7fb03',
+          600: '#95b300',
+          700: '#708803',
+          800: '#586a0a',
+          900: '#4a5a0d',
+        },
       },
     },
   },
